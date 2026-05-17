1c490d9412175878c80b43989dc039f87e618ad2
Chodisetty Dhanunjaya <chodisettydhanunjaya@gmail.com>
2026-05-17 15:26:37 +0530

    Time: 0 ms (100%), Space: 29.4 MB (90.97%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % (nums.size());
-        std::rotate(nums.rbegin(), nums.rbegin() + k, nums.rend());
+        std::rotate(nums.rbegin(), nums.rbegin() + k % (nums.size()), nums.rend());
     }
 };
