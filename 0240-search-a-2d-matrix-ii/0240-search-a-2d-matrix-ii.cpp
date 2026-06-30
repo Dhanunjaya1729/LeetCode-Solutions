58c8e1cb4949bd514a411afab277e6dc7ea7f83e
Chodisetty Dhanunjaya <chodisettydhanunjaya@gmail.com>
2026-06-30 17:07:43 +0530

    Time: 51 ms (69.57%), Space: 18.7 MB (67.8%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -10,9 +10,16 @@ public:
         int col = n - 1;
 
         while(row < m && col >= 0){
-            if(matrix[row][col] == target) return true;
-            else if(matrix[row][col] < target) row++;
-            else col--;
+
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                row++;
+            }
+            else{
+                col--;
+            }
         }
 
         return false;
