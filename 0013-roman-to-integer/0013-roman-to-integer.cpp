dc815701b976c2984e8bf41a98c3a215a23f9931
Chodisetty Dhanunjaya <chodisettydhanunjaya@gmail.com>
2026-07-04 16:05:38 +0530

    Time: 2 ms (78.42%), Space: 10.1 MB (77.75%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> m = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-
-        int n = s.size();
         int total = 0;
+        int prev_val = 0;
 
-        for(int i = 0; i < n; i++){
-            if(i + 1 < n && m[s[i]] < m[s[i + 1]]){
-                total -= m[s[i]];
+        for(char c: s|views::reverse){
+            int curr_val = 0;
+
+            switch(c){
+                case 'I': curr_val = 1; break;
+                case 'V': curr_val = 5; break;
+                case 'X': curr_val = 10; break;
+                case 'L': curr_val = 50; break;
+                case 'C': curr_val = 100; break;
+                case 'D': curr_val = 500; break;
+                case 'M': curr_val = 1000; break;
             }
-            else{
-                total += m[s[i]];
-            }
+
+            total = (curr_val < prev_val) ? total - curr_val: total + curr_val;
+            prev_val = curr_val;
         }
         return total;
     }
