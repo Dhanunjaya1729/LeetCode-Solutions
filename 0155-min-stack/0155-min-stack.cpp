96dfae62147bc1299671d52006c79a0da2579b15
Chodisetty Dhanunjaya <chodisettydhanunjaya@gmail.com>
2026-07-07 21:13:07 +0530

    Time: 55 ms (45.8%), Space: 151 MB (61.35%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,23 @@
 class MinStack {
 private:
     stack<pair<int, int>> st;
+
 public:
     MinStack() {}
-    
+
     void push(int value) {
-        if(st.empty()){
+        if (!st.empty()) {
+            st.push({value, min(value, st.top().second)});
+        } else {
             st.push({value, value});
         }
-        else{
-            st.push({value, min(value, st.top().second)});
-        }
     }
-    
-    void pop() {
-        st.pop();
-    }
-    
-    int top() {
-        return st.top().first;
-    }
-    
-    int getMin() {
-        return st.top().second;
-    }
+
+    void pop() { st.pop(); }
+
+    int top() { return st.top().first; }
+
+    int getMin() { return st.top().second; }
 };
 
 /**
