dd970c229cb18e5376b41182e8bca910875bf545
Chodisetty Dhanunjaya <chodisettydhanunjaya@gmail.com>
2026-07-08 16:01:04 +0530

    Time: 0 ms (100%), Space: 8.7 MB (38.87%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long val = (long long)dividend/divisor;
+        if(val > INT_MAX){
+            return INT_MAX;
+        }
+        else if(val < INT_MIN){
+            return INT_MIN;
+        }
+        return (int)val;
+    }
+};
