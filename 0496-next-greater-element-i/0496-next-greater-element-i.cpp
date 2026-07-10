fd07f3bf0893086a8492b157dc56bbe7039cb340
Chodisetty Dhanunjaya <chodisettydhanunjaya@gmail.com>
2026-07-10 12:27:22 +0530

    Time: 0 ms (100%), Space: 12.7 MB (76.05%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        vector<int> ans(m, -1);
-
-        for(int i = 0; i < m; i++){
-            stack<int> st;
-            for(int j = n - 1; j >= 0; j--){
-                int currentElement = nums2[j];
-                while(!st.empty() && st.top() <= currentElement){
-                    st.pop();
-                }
-                if(st.empty()){
-                    ans[i] = -1;
-                }
-                else{
-                    if(nums1[i] == nums2[j]){
-                        ans[i] = st.top();
-                        break;
-                    }
-                }
-                st.push(currentElement);
+        unordered_map<int,int> mpp;
+        stack<int> st;
+        for(int i = nums2.size() - 1; i>=0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
             }
+            if(st.empty()){
+                mpp[nums2[i]] = -1;
+            }
+            else{
+                mpp[nums2[i]] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+        
+        vector<int>ans;
+        for(int i = 0; i< nums1.size(); i++){
+           ans.push_back(mpp[nums1[i]]);
         }
         return ans;
     }
