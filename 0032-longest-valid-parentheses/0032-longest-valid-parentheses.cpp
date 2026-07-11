3c259992c4a1b3b5e2a3a6ae9d86be56e4996261
Chodisetty Dhanunjaya <chodisettydhanunjaya@gmail.com>
2026-07-11 15:28:41 +0530

    Time: 0 ms (100%), Space: 11.8 MB (52.46%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         stack<int> st;
-        st.push(-1);
         int maxLen = 0;
+        st.push(-1);
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
                 st.push(i);
-            }
-            else{
+            } else {
                 st.pop();
-                if(st.empty()){
+                if (st.empty()) {
                     st.push(i);
-                }
-                else{
+                } else {
                     maxLen = max(maxLen, i - st.top());
                 }
             }
